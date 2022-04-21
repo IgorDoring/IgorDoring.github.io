d72b95e10066717e10debadd5989e98df1b8ea70
IgorDoring <idoring@tutanota.com>
2022-04-21 18:27:53 -0300

    Perfil completed

--- index.php
+++ index.php
@@ -1,20 +1,58 @@
 <!DOCTYPE html>
-  <html>
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="css/style.css" />
+<html>
+<head>
+	<title>CV Igor Doring</title>
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
 
-    <body>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m4">
+				<div class="info-box perfil z-depth-3">
+					<img class="responsive-img" src="img/img branco.jpg">
+						<div class="info-text">
+
+							<h5>Igor Doring de Paula</h5>
+							<h6 class="valign-wrapper"><span class="material-icons">home</span> Centro, São Leopoldo - RS</h6>
+							<h6 class="valign-wrapper"><span class="material-icons">business_center</span> Front End Developer</h6>
+							<div class="divider"></div>
+							<p>Sou estudante de Análise e Desenvolvimento de Sistemas na Unisinos, atualmente trabalho na equipe de sites do setor de marketing da Unisinos. Tenho ótimo conhecimento em desenvolvimento WEB e sempre tento aperfoiçoar aquilo que já sei, assim como compreender aquilo que nunca soube.</p>
+							<div class="divider"></div>
+							<h5>Contato</h5>
+							<h6 class="valign-wrapper"><span class="material-icons">phone_android</span> +55 51 99336-0850</h6>
+							<h6 class="valign-wrapper"><span class="material-icons">email</span> igor.doring29@gmail.com</h6>
+						</div>
+
+				</div>
+			</div>
+			<div class="col s12 m8">
+				<div class="info-box experiencia">
+				</div>
+				<div class="info-box educacao">
+				</div>
+				<div class="row">
+					<div class="col s12 m6">
+						<div class="info-box habilidade">
+						</div>
+					</div>
+					<div class="col s12 m6">
+						<div class="info-box idioma">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
-      <!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
+
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
