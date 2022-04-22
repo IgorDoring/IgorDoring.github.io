83134afebbb031be4ce40329ad3eec51036eb129
IgorDoring <idoring@tutanota.com>
2022-04-22 15:37:07 -0300

    Ajustes finais do lado esquerdo

--- index.php
+++ index.php
@@ -17,24 +17,58 @@
 			<div class="col s12 m4">
 				<div class="info-box perfil z-depth-3">
 					<img class="responsive-img" src="img/img branco.jpg">
-						<div class="info-text">
-							<h5>Igor Doring de Paula</h5>
-							<h6 class="valign-wrapper"><span class="material-icons">home</span> Centro, São Leopoldo - RS</h6>
-							<h6 class="valign-wrapper"><span class="material-icons">business_center</span> Front End Developer</h6>
-							<div class="divider"></div>
-							<p>Sou estudante de Análise e Desenvolvimento de Sistemas na Unisinos, atualmente trabalho na equipe de sites do setor de marketing da Unisinos. Tenho ótimo conhecimento em desenvolvimento WEB e sempre tento aperfeiçoar aquilo que já sei, assim como compreender aquilo que nunca soube.</p>
-							<div class="divider"></div>
-							<h5>Contato</h5>
-							<h6 class="valign-wrapper"><span class="material-icons">phone_android</span> +55 51 99336-0850</h6>
-							<h6 class="valign-wrapper"><span class="material-icons">email</span> igor.doring29@gmail.com</h6>
-						</div>
+				</div>
 
+				<div class="info-box perfil z-depth-3">
+					<div class="info-text">
+						<h5>Igor Doring de Paula</h5>
+						<h6 class="valign-wrapper"><span class="material-icons">business_center</span> Front End Developer</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">home</span> Centro, São Leopoldo - RS</h6>
+						<div class="divider"></div>
+						<p>Sou estudante de Análise e Desenvolvimento de Sistemas na Unisinos, atualmente trabalho na equipe de sites do setor de marketing da Unisinos. Tenho ótimo conhecimento em desenvolvimento WEB e sempre tento aperfeiçoar aquilo que já sei, assim como compreender aquilo que nunca soube.</p>
+						<div class="divider"></div>
+						<h5>Contato</h5>
+						<h6 class="valign-wrapper"><span class="material-icons">phone_android</span> +55 51 99336-0850</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">email</span> igor.doring29@gmail.com</h6>
+					</div>
+				</div>
+
+				<div class="info-box habilidade z-depth-3">
+					<div class="info-text">
+						<h5>Qualificações</h5>
+						<p>HTML, CSS, JS, PHP</p>
+						<div class="skill-bar"><p class="center-align" style="width: 90%;">90%</p></div>
+						<p>MySQL</p>
+						<div class="skill-bar"><p class="center-align" style="width: 80%;">80%</p></div>
+						<p>Java</p>
+						<div class="skill-bar"><p class="center-align" style="width: 60%;">60%</p></div>
+						<p>C, C++</p>
+						<div class="skill-bar"><p class="center-align" style="width: 50%;">50%</p></div>
+						<p>Git</p>
+						<div class="skill-bar"><p class="center-align" style="width: 40%;">40%</p></div>
+						<p>Wordpress</p>
+						<div class="skill-bar"><p class="center-align" style="width: 70%;">70%</p></div>
+						<p>CodeIgniter</p>
+						<div class="skill-bar"><p class="center-align" style="width: 40%;">40%</p></div>
+					</div>
+				</div>
+
+				<div class="info-box idioma z-depth-3">
+					<div class="info-text">
+						<h5>Idiomas</h5>
+						<p>Português</p>
+						<div class="skill-bar"><p class="center-align">100%</p></div>
+						<p>English</p>
+						<div class="skill-bar"><p class="center-align" style="width: 97%;">97%</p></div>
+						<p>Español</p>
+						<div class="skill-bar"><p class="center-align" style="width: 35%;">35%</p></div>
+					</div>
 				</div>
 			</div>
 			<div class="col s12 m8">
 				<div class="info-box experiencia z-depth-3">
 					<div class="info-text">
-						<h4 class="valign-wrapper"><span class="material-icons">work_history</span> Experiência de Trabalho</h4>
+						<h4 class="valign-wrapper"><span class="material-icons">work_history</span> Experiência Profissional</h4>
 						<div class="divider"></div>
 						<h6 class="valign-wrapper"><span class="material-icons">work</span> Desenvolvedor Front-End</h6>
 						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2021 - <span class="atual">Atual</span></h6>
@@ -54,7 +88,7 @@
 				</div>
 				<div class="info-box educacao z-depth-3">
 					<div class="info-text">
-						<h4 class="valign-wrapper"><span class="material-icons">school</span> Educação</h4>
+						<h4 class="valign-wrapper"><span class="material-icons">school</span> Formação</h4>
 						<div class="divider"></div>
 						<h6 class="valign-wrapper"><span class="material-icons">class</span> Análise e Desenvolvimento de Sistemas</h6>
 						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2021 - <span class="atual">Atual</span></h6>
@@ -75,16 +109,6 @@
 						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2016 - 2019</h6>
 						<h6 class="valign-wrapper"><span class="material-icons">business</span> Escola Técnica Estadual Monteiro Lobato</h6>
 						<p>Completo.</p>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col s12 m6">
-						<div class="info-box habilidade z-depth-3">
-						</div>
-					</div>
-					<div class="col s12 m6">
-						<div class="info-box idioma z-depth-3">
-						</div>
 					</div>
 				</div>
 			</div>
