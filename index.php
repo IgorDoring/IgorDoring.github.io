f32acf41287422dad28acea7acf8cfa733031de1
IgorDoring <idoring@tutanota.com>
2022-04-22 12:57:06 -0300

    Experiencia de Trabalho concluida

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 							<h6 class="valign-wrapper"><span class="material-icons">home</span> Centro, São Leopoldo - RS</h6>
 							<h6 class="valign-wrapper"><span class="material-icons">business_center</span> Front End Developer</h6>
 							<div class="divider"></div>
-							<p>Sou estudante de Análise e Desenvolvimento de Sistemas na Unisinos, atualmente trabalho na equipe de sites do setor de marketing da Unisinos. Tenho ótimo conhecimento em desenvolvimento WEB e sempre tento aperfoiçoar aquilo que já sei, assim como compreender aquilo que nunca soube.</p>
+							<p>Sou estudante de Análise e Desenvolvimento de Sistemas na Unisinos, atualmente trabalho na equipe de sites do setor de marketing da Unisinos. Tenho ótimo conhecimento em desenvolvimento WEB e sempre tento aperfeiçoar aquilo que já sei, assim como compreender aquilo que nunca soube.</p>
 							<div class="divider"></div>
 							<h5>Contato</h5>
 							<h6 class="valign-wrapper"><span class="material-icons">phone_android</span> +55 51 99336-0850</h6>
@@ -34,7 +34,22 @@
 			<div class="col s12 m8">
 				<div class="info-box experiencia z-depth-3">
 					<div class="info-text">
-						<h4>Experiência</h4>	
+						<h4 class="valign-wrapper"><span class="material-icons">work_history</span> Experiência de Trabalho</h4>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">work</span> Desenvolvedor Front-End</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2021 - <span class="atual">Atual</span></h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> Universidade do Vale do Rio dos Sinos</h6>
+						<p>Sou responsável pela manutenção de páginas dentro do domínio, escopo e marca da Universidade. Eventualmente, fica ao meu cargo o desenvolvimento de landing pages de programas da Universidade.</p>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">work</span> Estagiário Desenvolvedor WEB Full-stack</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2019-2021</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> AGEXCOM</h6>
+						<p>Fui responsável pela criação de sites e aplicações web. Também fui responsável pela manutenção do <a href="http://mescla.cc/o-mescla/"><b>Portal Mescla</b></a>.</p>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">work</span> Auxiliar</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2019</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> E.T.E Monteiro Lobato</h6>
+						<p>Fui responsável pela manutenção de computadores, assim como auxílio aos funcionários da escola quando fosse necessário.</p>
 					</div>
 				</div>
 				<div class="info-box educacao z-depth-3">
