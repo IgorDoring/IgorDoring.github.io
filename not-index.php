b2e505bd2fb4bc5aee8c5c20be6577db943f4f9a
IgorDoring <idoring@tutanota.com>
2022-05-17 20:31:04 -0300

    creating html file

---
+++ not-index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CV Igor Doring</title>
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col s12 m4">
+				<img class="responsive-img perfil-img" src="img/foto-temp.jpg">
+				<div class="info-box perfil z-depth-3">
+					<div class="info-text">
+						<h5>Igor Doring de Paula</h5>
+						<h6 class="valign-wrapper"><span class="material-icons">business_center</span> Front End Developer</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">home</span> Centro, São Leopoldo - RS</h6>
+						<div class="divider"></div>
+						<p>Sou estudante de Análise e Desenvolvimento de Sistemas na Unisinos, atualmente trabalho na equipe de sites do setor de marketing da Unisinos. Tenho ótimo conhecimento em desenvolvimento WEB e sempre tento aperfeiçoar aquilo que já sei, assim como compreender aquilo que nunca soube.</p>
+						<div class="divider"></div>
+						<h5>Contato</h5>
+						<h6 class="valign-wrapper"><span class="material-icons">phone_android</span> +55 51 99336-0850</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">email</span> igor.doring29@gmail.com</h6>
+					</div>
+				</div>
+				<div class="info-box qualificacoes z-depth-3">
+					<div class="info-text">
+						<h5 class="valign-wrapper expandable">
+							<span class="material-icons">stars</span>
+							Qualificações
+							<span class="material-icons md-36">expand_more</span>
+						</h5>
+						<div class="divider"></div>
+						<p>HTML, CSS, JS, PHP</p>
+						<div class="skill-bar"><p class="center-align" style="width: 90%;">90%</p></div>
+						<p>MySQL</p>
+						<div class="skill-bar"><p class="center-align" style="width: 80%;">80%</p></div>
+						<p>Java</p>
+						<div class="skill-bar"><p class="center-align" style="width: 60%;">60%</p></div>
+						<p>C, C++</p>
+						<div class="skill-bar"><p class="center-align" style="width: 50%;">50%</p></div>
+						<p>Git</p>
+						<div class="skill-bar"><p class="center-align" style="width: 40%;">40%</p></div>
+						<p>Wordpress</p>
+						<div class="skill-bar"><p class="center-align" style="width: 70%;">70%</p></div>
+						<p>CodeIgniter</p>
+						<div class="skill-bar"><p class="center-align" style="width: 40%;">40%</p></div>
+					</div>
+				</div>
+				<div class="info-box idioma z-depth-3">
+					<div class="info-text">
+						<h5 class="valign-wrapper expandable">
+							<span class="material-icons">language</span>
+							Idiomas
+							<span class="material-icons md-36">expand_more</span> 
+						</h5>
+						<div class="divider"></div>
+						<p>Português</p>
+						<div class="skill-bar"><p class="center-align">100%</p></div>
+						<p>English</p>
+						<div class="skill-bar"><p class="center-align" style="width: 97%;">97%</p></div>
+						<p>Español</p>
+						<div class="skill-bar"><p class="center-align" style="width: 15%;">15%</p></div>
+					</div>
+				</div>
+			</div>
+			<div class="col s12 m8">
+				<div class="info-box experiencia z-depth-3">
+					<div class="info-text">
+						<h5 class="valign-wrapper expandable">
+							<span class="material-icons">work_history</span> 
+							Experiência<br class="hide-on-med-and-up"> Profissional
+							<span class="material-icons md-36">expand_more</span> 
+						</h5>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">work</span> Desenvolvedor Front-End</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2021 - <span class="atual">Atual</span></h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> Universidade do Vale do Rio dos Sinos</h6>
+						<p>Sou responsável pela manutenção de páginas dentro do domínio, escopo e marca da Universidade. Eventualmente, fica ao meu cargo o desenvolvimento de landing pages de programas da Universidade.</p>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">work</span> Estagiário Desenvolvedor WEB Full-stack</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2019-2021</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> AGEXCOM</h6>
+						<p>Fui responsável pela criação de sites e aplicações web. Também fui responsável pela manutenção do <a href="http://mescla.cc/o-mescla/" style="color: black;"><b>Portal Mescla</b></a>.</p>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">work</span> Auxiliar</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2019</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> E.T.E Monteiro Lobato</h6>
+						<p>Fui responsável pela manutenção de computadores, assim como auxílio aos funcionários da escola quando fosse necessário.</p>
+					</div>
+				</div>
+				<div class="info-box formacao z-depth-3">
+					<div class="info-text">
+						<h5 class="valign-wrapper expandable">
+							<span class="material-icons">school</span> 
+							Formação
+							<span class="material-icons md-36">expand_more</span> 
+						</h5>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">class</span> Análise e Desenvolvimento de Sistemas</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2021 - <span class="atual">Atual</span></h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> Universidade do Vale do Rio dos Sinos</h6>
+						<p>Em andamento. Pretendo concluir antes do fim do ano de 2025.</p>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">class</span> Ciência da Computação</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2021</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> Universidade do Vale do Rio dos Sinos</h6>
+						<p>Incompleto. Resolvi parar pois percebi que tinha mais interesse no curso de análise e desenvolvimento de sistemas.</p>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">class</span> Segurança da Informação</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2019 - 2021</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> Universidade do Vale do Rio dos Sinos</h6>
+						<p>Incompleto. Resolvi parar porque era uma daquelas situações onde eu só teria a oportunidade de arranjar emprego apenas depois que acabasse a o curso.</p>
+						<div class="divider"></div>
+						<h6 class="valign-wrapper"><span class="material-icons">class</span> Técnico em Informática</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">calendar_month</span> 2016 - 2019</h6>
+						<h6 class="valign-wrapper"><span class="material-icons">business</span> Escola Técnica Estadual Monteiro Lobato</h6>
+						<p>Completo.</p>
+					</div>
+				</div>
+				<div class="info-box portfolio z-depth-3">
+					<div class="info-text">
+						<h5 class="valign-wrapper expandable">
+							<span class="material-icons">source</span>
+							Portfólio
+							<span class="material-icons md-36">expand_more</span> 
+						</h5>
+						<p>Conheça alguns projetos com quais eu já estive envolvido.</p>
+						<div class="divider"></div>
+						<div class="row">
+							<div class="col s12 m6 portfolio-card">
+								<a href="https://dayamoraes.com.br/">
+									<p class="center-align">Daya Moraes</p>
+									<img class="responsive-img" src="img/dayamoraes-agexcom.jpg">
+								</a>
+							</div>
+							<div class="col s12 m6 portfolio-card">
+								<a href="https://unisinos.apprbs.com.br/dtt">
+									<p class="center-align">Digital Transformation Technologies</p>
+									<img class="responsive-img" src="img/dtt-unisinos.jpg">
+								</a>
+							</div>
+							<div class="col s12 m6 portfolio-card">
+								<a href="http://www.unisinos.br/sisgen/">
+									<p class="center-align">Sisgen Unisinos</p>
+									<img class="responsive-img" src="img/sisgen-unisinos.jpg">
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="js/jquery36.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+</body>
+</html>
